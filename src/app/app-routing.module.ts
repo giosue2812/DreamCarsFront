5898120d379ec46202ec08e115a665404e4f21d3
giosue <giosue_liuzzo@hotmail.be>
2020-04-16 16:39:19 +0200

    Formulaire et gestion du Tokken pourn la connexion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'connexion',loadChildren:() => import('../app/features/connexion/connexion.module').then(m => m.ConnexionModule)},
+  {path: 'home',loadChildren:()=> import('../app/features/home/home.module').then(m => m.HomeModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
